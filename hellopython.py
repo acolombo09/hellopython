e9192d846f8f50b8e9f0bae072ece75255dda3f7
acolombo09 <acolombo0911@gmail.com>
2023-12-20 14:49:19 +0100

    arrays_added

--- hellopython.py
+++ hellopython.py
@@ -1,8 +1,10 @@
+# for loop
 for i in range(3):
   print("Hello Phyton")
 
 i = 6
 
+# while loop
 while i > 0:
   print("I am working")
   i = i - 1
@@ -10,14 +12,46 @@ print("I stopped working")
 
 i = 28
 
+# if statements
 if i >= 18:
   print("Okeeey let's go")
 else:
   print("MI DISSOCIO")
 
+# declaring a function
 def add_two_numbers(x, y):
-    total = x + y
-    return total
+  total = x + y
+  return total
 
 output = add_two_numbers(1, 2)
 print(output)
+
+# arrays
+people = ['John' , 'Jane' , 'Joe']
+
+people.append('Jackson')
+
+print(people)
+
+name = people.pop()
+
+print(name)
+print(people)
+
+people.remove('Jane')
+
+print(people)
+
+people.append(9)
+
+print(people)
+
+# append without append function
+
+people_tuple = ('Jamie', 'Jackie')
+
+print(people_tuple)
+
+people_tuple = ('Jamie', 'Jackie', 'Jason')
+
+print(people_tuple)

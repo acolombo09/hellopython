970ba0624e855677331586b70624e723b789bb73
acolombo09 <acolombo0911@gmail.com>
2023-12-20 12:27:30 +0100

    playing

--- hellopython.py
+++ hellopython.py
@@ -1,9 +1,16 @@
-for i in range(10):
+for i in range(3):
   print("Hello Phyton")
 
-i = 10
+i = 6
 
 while i > 0:
   print("I am working")
   i = i - 1
 print("I stopped working")
+
+i = 28
+
+if i >= 18:
+  print("Okeeey let's go")
+else:
+  print("MI DISSOCIO")

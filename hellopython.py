58674c8a53ea1e680b6ea1a2199d872e419e7dca
acolombo09 <acolombo0911@gmail.com>
2023-12-20 15:10:47 +0100

    added_more_for & while_loops

--- hellopython.py
+++ hellopython.py
@@ -1,41 +1,4 @@
-# for loop
-for i in range(3):
-  print("Hello Phyton")
-
-i = 6
-
-# while loop
-while i > 0:
-  print("I am working")
-  i = i - 1
-print("I stopped working")
-
-i = 28
-
-# if statements
-if i >= 18:
-  print("Okeeey let's go")
-else:
-  print("MI DISSOCIO")
-
-age = 28
-
-if age < 19:
-  print('The person is a child')
-elif age < 70:
-  print('The person is an adult')
-else:
-  print('The person is a senior citizen')
-
-# declaring a function
-def add_two_numbers(x, y):
-  total = x + y
-  return total
-
-output = add_two_numbers(1, 2)
-print(output)
-
-# arrays
+# arrays ----------------------------------------
 people = ['John' , 'Jane' , 'Joe']
 
 people.append('Jackson')
@@ -55,7 +18,7 @@ people.append(9)
 
 print(people)
 
-# append without append function
+# append without append function ----------------------------------------
 
 people_tuple = ('Jamie', 'Jackie')
 
@@ -64,3 +27,62 @@ print(people_tuple)
 people_tuple = ('Jamie', 'Jackie', 'Jason')
 
 print(people_tuple)
+
+# if statements ----------------------------------------
+i = 28
+
+if i >= 18:
+  print("Okeeey let's go")
+else:
+  print("MI DISSOCIO")
+
+age = 28
+
+if age < 19:
+  print('The person is a child')
+elif age < 70:
+  print('The person is an adult')
+else:
+  print('The person is a senior citizen')
+
+# for loop ----------------------------------------
+for i in range(3):
+  print("Hello Phyton")
+
+i = 6
+
+peoples = ['John', 'Jane', 'Joe']
+
+for name in peoples:
+  print(name)
+
+# while loop ----------------------------------------
+while i > 0:
+  print("I am working")
+  i = i - 1
+print("I stopped working")
+
+user_input = input('random ')
+
+while user_input != 'exit':
+  user_input = input('>>> ')
+  print('You entered: ', user_input)
+
+print('Goodbye!')
+
+# declaring a function -------------------------
+def add_two_numbers(x, y):
+  total = x + y
+  return total
+
+output = add_two_numbers(1, 2)
+print(output)
+
+def print_sq_value(numbers):
+  for n in numbers:
+    sq = n*n
+    print('Square of', n, 'is', sq)
+
+numbers = [1, 2, 3, 4, 5]
+
+print_sq_value(numbers)
